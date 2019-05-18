4685caf2912402df7c5ded453049d4421be9ce2a
Doug Loomer <loomerds@gmail.com>
2019-05-18 13:58:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-var main = document.getElementById('#main');
-console.log(main);
+window.onload=function(){
+var main = document.getElementById('main');
 if(main) {
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
+});
+}
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
 });
 }
