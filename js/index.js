9c93ca6367df906c0c3bd1aef4250fa326a8fb94
Doug Loomer <loomerds@gmail.com>
2019-05-18 14:42:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -16,11 +16,20 @@ window.onload=function(){
 
   let divs = document.querySelectorAll('div');
 
-function bubble(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
-}
+  function bubble(e) {
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+  }
 
-for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener('click', bubble);
-}
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+  }
+
+  function capture(e) {
+    console.log(this.firstChild.nodeValue.trim() + ' captured');
+  }
+   
+  for (let i = 0; i < divs.length; i++) {
+    // set the third argument to `true`!
+    divs[i].addEventListener('click', capture, true);
+  }
 }
