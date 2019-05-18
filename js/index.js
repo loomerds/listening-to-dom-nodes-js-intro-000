db0136e70d475314fdc875d8903216c16ae4db51
Doug Loomer <loomerds@gmail.com>
2019-05-18 14:25:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ window.onload=function(){
   const input = document.querySelector('input');
 
   input.addEventListener('keydown', function(e) {
-    console.log(e.key);
+    if (e.key === "g") {
+    return e.preventDefault()
+    } else {
+      console.log(e.key)
+    }
   });
 }
